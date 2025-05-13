765684ef0f29202035664154696616d42af3a5ca
pablichoka <pablikoruab98@gmail.com>
2025-05-14 00:34:26 +0200

    feat(layout): update metadata and add favicon

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import LanguageSelector from "@components/custom/LanguageSelector";
 import LoginForm from "@components/custom/LoginForm";
@@ -17,11 +17,10 @@ import {
   TabsTrigger,
 } from "@components/shadcn/ui/tabs";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 export default function Home() {
-  const t = useTranslations("frontpage"); 
-
-  console.log('mapId', process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID);
+  const t = useTranslations("frontpage");
 
   return (
     <>
@@ -47,35 +46,61 @@ export default function Home() {
         </div>
 
         <div className="w-2/5 pt-4 flex flex-col items-center bg-primary text-black">
-          <div className="w-full h-1/2 justify-start max-w-sm p-8 rounded-lg items-center">
+          <div className="w-full h-3/5 justify-start max-w-sm p-2 rounded-lg items-center overflow-y-auto">
+            <div className="flex flex-col items-center">
+              <Image
+                src="/images/logo.png"
+                alt="Logo"
+                width={110}
+                height={110}
+                className="rounded-lg shadow-2xl mb-4 p-2 bg-white"
+              />
+            </div>
             <Tabs defaultValue="account" className="w-full">
               <TabsList className="grid w-full grid-cols-2">
                 <TabsTrigger value="account">
-                  {t("LoginForm.loginTab")}
+                  {t("loginForm.loginTab")}
                 </TabsTrigger>
                 <TabsTrigger value="password">
-                  {t("LoginForm.signUpTab")}
+                  {t("loginForm.signUpTab")}
                 </TabsTrigger>
               </TabsList>
               <TabsContent value="account">
                 <LoginForm />
               </TabsContent>
               <TabsContent value="password">
-                <p>{t("LoginForm.signUpPlaceholder")}</p>
+                {/* TODO create signup form */}
               </TabsContent>
             </Tabs>
           </div>
-          <div className="rounded-lg items-center">
+          <div className="h-2/5 w-full flex flex-col justify-center items-center ">
+            <div>
+              <h1 className="text-lg font-bold text-center mb-4">
+                {t("map.title")}
+              </h1>
+            </div>
             <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
               <Map
-                style={{ width: "40vw", height: "50vh", borderRadius: "1rem", overflow: "hidden" }}
-                defaultCenter={{ lat: 38.05549193859103, lng: -1.217749291324227 }}
-                defaultZoom={19}
+                style={{
+                  width: "30vw",
+                  height: "30vh",
+                  borderRadius: "1rem",
+                  overflow: "hidden",
+                  marginBottom: "1rem",
+                }}
+                defaultCenter={{
+                  lat: 38.05549193859103,
+                  lng: -1.217749291324227,
+                }}
+                defaultZoom={15}
                 gestureHandling={"greedy"}
+                disableDefaultUI={true}
                 mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_MAP_ID}
                 mapTypeId="terrain"
               >
-                <AdvancedMarker position={{ lat: 38.05549193859103, lng: -1.217749291324227 }} />
+                <AdvancedMarker
+                  position={{ lat: 38.05549193859103, lng: -1.217749291324227 }}
+                />
               </Map>
             </APIProvider>
           </div>
