e53e92e22f517dab773cf6d31aa3753c2e6d8ddf
pablichoka <pablikoruab98@gmail.com>
2025-05-14 17:09:30 +0200

    feat(signup): implement signup form with validation and translations

---
+++ src/components/custom/SignUpForm.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import { Button } from "@components/shadcn/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@components/shadcn/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import "@styles/globals.css";
+import { useTranslations } from "next-intl";
+
+const signUpFormValidationSchema = (t: (key: string) => string) =>
+  z
+    .object({
+      username: z.string().min(3, { message: t("usernameRequired") }),
+      password: z
+        .string()
+        .min(8, { message: t("passwordMinLength") })
+        .max(30, { message: t("passwordMaxLength") })
+        .regex(/[a-z]/, { message: t("passwordLowercase") })
+        .regex(/[A-Z]/, { message: t("passwordUppercase") })
+        .regex(/[0-9]/, { message: t("passwordNumber") })
+        .regex(/[^a-zA-Z0-9]/, { message: t("passwordSpecialChar") }),
+      confirmPassword: z
+        .string()
+        .min(1, { message: t("confirmPasswordRequired") }),
+      email: z.string().email({ message: t("emailRequired") }),
+      fullName: z.string().min(2, { message: t("fullNameRequired") }),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: t("passwordsDoNotMatch"),
+      path: ["confirmPassword"],
+    });
+
+type SignUpFormValues = z.infer<ReturnType<typeof signUpFormValidationSchema>>;
+
+export default function SignUpForm() {
+  const t = useTranslations("frontpage.signupForm");
+  const currentSchema = signUpFormValidationSchema(t);
+
+  const form = useForm<SignUpFormValues>({
+    resolver: zodResolver(currentSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+      confirmPassword: "",
+      email: "",
+      fullName: "",
+    },
+    mode: "onChange",
+  });
+
+  function onSubmit(data: SignUpFormValues) {
+    console.log("SignUp data:", data);
+  }
+
+  return (
+    <>
+      <div className="flex-grow rounded-lg p-2 background mt-2 w-full">
+        <Form {...form}>
+          <FormField
+            control={form.control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("fullNameLabel")}</FormLabel>
+                <FormControl>
+                  <input
+                    {...field}
+                    className="border-1 border-gray-500 rounded-md p-1 w-full"
+                  />
+                </FormControl>
+                <FormMessage className="text-xs" />
+              </FormItem>
+            )}
+          />
+          <div className="mb-2" />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("emailLabel")}</FormLabel>
+                <FormControl>
+                  <input
+                    {...field}
+                    type="email"
+                    className="border-1 border-gray-500 rounded-md p-1 w-full"
+                  />
+                </FormControl>
+                <FormMessage className="text-xs" />
+              </FormItem>
+            )}
+          />
+          <div className="mb-2" />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("usernameLabel")}</FormLabel>
+                <FormControl>
+                  <input
+                    {...field}
+                    className="border-1 border-gray-500 rounded-md p-1 w-full"
+                  />
+                </FormControl>
+                <FormMessage className="text-xs" />
+              </FormItem>
+            )}
+          />
+          <div className="mb-2" />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("passwordLabel")}</FormLabel>
+                <FormControl>
+                  <input
+                    {...field}
+                    type="password"
+                    className="border-1 border-gray-500 rounded-md p-1 w-full"
+                  />
+                </FormControl>
+                <FormMessage className="text-xs" />
+              </FormItem>
+            )}
+          />
+          <div className="mb-2" />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("confirmPasswordLabel")}</FormLabel>
+                <FormControl>
+                  <input
+                    {...field}
+                    type="password"
+                    className="border-1 border-gray-500 rounded-md p-1 w-full"
+                  />
+                </FormControl>
+                <FormMessage className="text-xs" />
+              </FormItem>
+            )}
+          />
+        </Form>
+      </div>
+      <Button
+        onClick={form.handleSubmit(onSubmit)}
+        className="w-full mt-4 bg-background text-foreground"
+      >
+        {t("signUpButton")}
+      </Button>
+    </>
+  );
+}
